63969dcfc8717f8db44d3d16455d9690ff2d7d79
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2024-04-15 18:01:26 -0300

    Pausado o Html e iniciado o React

---
+++ entendendoOHTMLECSS/cores/coresHEX/projeto61/cores_hex.md
@@ -0,0 +1,17 @@
+# Cores Hexadecimais
+
+    R = RED     (Vermelho)
+    G = GREEN   (Verde)
+    B = BLUE    (Azul)
+
+    0-255
+    
+
+    2 = vermelho    = 0-9 e A ao F
+    2 = verde       = 0-9 e A ao F
+    2 = azul        = 0-9 e A ao F
+
+    rgb()
+    rgba()
+
+    #
---
+++ fundamentosDoReact.js/os3+1ConceitosMaisImportantesDoReact/componentes/script.js
@@ -0,0 +1,20 @@
+const Topic = () => {
+  const increaseCount = () => {
+    console.log('increase count...');
+  }
+
+  return <div>PHP (1) <button onClick={increaseCount}>+1</button></div>
+}
+
+const App = () => {
+  return (
+    <>
+      <h1>Vota Tópico</h1>
+      <Topic />
+      <Topic />
+      <Topic />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
