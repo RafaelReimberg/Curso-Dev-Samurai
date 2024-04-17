740da149abd9839d276137ed1c7003ec88233a3f
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2024-04-17 18:03:23 -0300

    realizando os testes

---
+++ fundamentosDoReact.js/ambienteDeDesenvolvimento/criandoUmaAplicacaoReact/my-app-react-2/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    node: true,
+    commojs: true,
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks', 'testing-library', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: './',
+      },
+    ],
+  }
+}
--- fundamentosDoReact.js/ambienteDeDesenvolvimento/criandoUmaAplicacaoReact/my-app-react-2/src/App.js
+++ fundamentosDoReact.js/ambienteDeDesenvolvimento/criandoUmaAplicacaoReact/my-app-react-2/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TESTANDO React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>App - Dev Reimberg!!</h1>
+    <button>Clique aqui!!</button>
+      
+    </>
+);
 }
 
 export default App;
