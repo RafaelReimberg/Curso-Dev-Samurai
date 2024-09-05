3d37928948decdaedaa6e157ab928d7ba3f50295
Rafael Marcelo Reimberg <raaafael.reimberg@hotmail.com>
2024-09-04 23:13:25 -0300

    Finalizado a sintaxe do projeto import/export

---
+++ node.jsAvancado/configurandoUmEstruturaDeProjetoModerna/eslintPrettierEEditorConfig/src/app/controllers/CustomersController.js
@@ -0,0 +1,64 @@
+let customers = [
+  { id: 1, name: "DEV Reimberg", site: "http://devreimberg.com.br" },
+  { id: 2, name: "ADM Natalia", site: "http://admnatalia.com" },
+  { id: 3, name: "TOY Gabriel", site: "http://toygabriel.com.br" },
+  { id: 4, name: "TOY Luiza", site: "http://toyluiza.com.br" },
+];
+
+class CustomersController {
+  // Listagem dos Customers
+  index(req, res) {
+    return res.json(customers);
+  }
+
+  // Recupera um Customers
+  show(req, res) {
+    const id = parseInt(req.params.id);
+    const customer = customers.find((item) => item.id === id);
+    const status = customer ? 200 : 404;
+
+    console.debug("GET :: /customers/:id", customer);
+
+    return res.status(status).json(customer);
+  }
+
+  // Cria um novo Customers
+  create(req, res) {
+    const { name, site } = req.body;
+    const id = customers[customers.length - 1].id + 1;
+
+    const newCustomer = { id, name, site };
+    customers.push(newCustomer);
+
+    return res.status(201).json(newCustomer);
+  }
+
+  // Atualiza um Customers
+  update(req, res) {
+    const id = parseInt(req.params.id);
+    const { name, site } = req.body;
+
+    const index = customers.findIndex((item) => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if (index >= 0) {
+      customers[index] = { id: parseInt(id), name, site };
+    }
+
+    return res.status(status).json(customers[index]);
+  }
+  // Exclui um Customers
+  destroy(req, res) {
+    const id = parseInt(req.params.id);
+    const index = customers.findIndex((item) => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if (index >= 0) {
+      customers.splice(index, 1);
+    }
+
+    return res.status(status).json();
+  }
+}
+
+export default new CustomersController();
