4775eac948086b23e09232736640a83e31237520
Rafael Marcelo Reimberg <raaafael.reimberg@hotmail.com>
2024-10-15 21:33:51 -0300

    iniciando a criacao do custormes

--- node.jsAvancado/ORMEBancoDeDados/criandoOModelCustomer/src/config/database.js
+++ node.jsAvancado/ORMEBancoDeDados/criandoOModelCustomer/src/config/database.js
@@ -2,7 +2,9 @@ module.exports = {
   dialect: "postgres",
   host: "localhost",
   username: "postgres",
+  // username: "empresarial",
   password: "secret",
+  // password: "Rafa@1906",
   database: "develop",
   define: {
     timestamp: true, // cria duas colunas: createdAt e updatedAt
